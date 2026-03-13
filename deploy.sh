dafaed223a73fb9b6f90eee45c2886e4da680a85
Sumit Ranjan Jha <sumitjhawiki@gmail.com>
2026-03-13 21:15:05 +0530

    fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,23 @@
 set -e
 echo "Start"
 cd /home/ubuntu/flask-react-template
-git pull origin main
+git fetch origin main
+git reset --hard origin/main
 
 # React Frontend
-npm ci
-npm run build
-sudo cp -r dist/* /var/www/html/
+NODE_OPTIONS="--max-old-space-size=2048" npm ci
+NODE_OPTIONS="--max-old-space-size=2048" npm run build
+sudo cp -r dist/public/* /var/www/html/
 echo "Frontend Deployed"
 
 # Python Backend
 python3 -m venv venv
 venv/bin/pip install pipenv
-venv/bin/pipenv install 
+venv/bin/pipenv install
 echo "Backend done"
 
 pkill gunicorn || true
-~/.local/share/virtualenvs/flask-react-template-9ALZKhU7/bin/gunicorn --bind unix:/tmp/app.sock app:app --daemon
+venv/bin/pipenv run gunicorn --bind unix:/tmp/app.sock app:app --daemon
 echo "Gunicorn start"
 
 sudo nginx -s reload
